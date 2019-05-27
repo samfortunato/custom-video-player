b5f32da270e74835cf36369ffcd69fcb4eedff0a
Samuel Fortunato <samfortunato@gmail.com>
2019-05-27 14:25:41 -0400

    Fix time calculation mistake

--- src/js/util/time_formatting.js
+++ src/js/util/time_formatting.js
@@ -1,6 +1,6 @@
 export const secondsToHHMMSS = (seconds) => {
   const amtMilliseconds = seconds * 1000;
-  const secondsAsDate = new Date(amtMilliseconds * 1000);
+  const secondsAsDate = new Date(amtMilliseconds);
   const hhMMSSString = secondsAsDate.toISOString().substring(11, 19);
 
   return hhMMSSString;
